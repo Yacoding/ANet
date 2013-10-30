260e305eaa639d7423e7817ece5e2ea2aa1db7fb
superzhaofengcao <superzhaofengcao@gmail.com>
2013-10-30 18:06:08 +0800

    添加blog的models-Posts

--- ANet/blog/models.py
+++ ANet/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Posts(models.Model):
+	content = models.TextField()
+	excerpt = models.TextField()
+	title   = models.CharField(max_length=100)
+	public_status = models.BooleanField()
+	comment_status = models.BooleanField()
+	post_time = models.DateTimeField(auto_now_add=True)
+	update_time = models.DateTimeField(auto_now=True)
+	author = models.ForeignKey(User)
+
--- ANet/blog/views.py
+++ ANet/blog/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+#-*-cofing:utf8-*-
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.shortcuts import render_to_response
+
+
+'''blog首页处理'''
+def index(request):
+	pass
