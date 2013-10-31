83f0851f9e00c3710025a2df1eea1a21f2b0cc5a
superzhaofengcao <superzhaofengcao@gmail.com>
2013-10-31 22:34:08 +0800

    css文件设置

--- ANet/ANet/urls.py
+++ ANet/ANet/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+import ANet
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^site_medias/(?P<path>.*)$','django.views.static.serve',{'document_root':ANet.settings.STATICFILES_DIRS, 'show_indexes': True}),
     url(r'^register/$', 'blog.views.register'),
     url(r'^blog/$', 'blog.views.blog'),
     url(r'^userinfo/$', 'blog.views.userinfo'),
