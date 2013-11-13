2d7203e8c7104af25a78e47366b5dd2072ed11bc
superzhaofengcao <superzhaofengcao@gmail.com>
2013-11-13 09:02:13 +0800

    添加新应用todo task list 1%

--- ANet/ANet/settings.py
+++ ANet/ANet/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'blog',
     'tinymce',
+    'todo',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- ANet/todo/models.py
+++ ANet/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class DateTime(models.Model):
+	datetime = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return unicode(self.datetime)
+
+class Item(models.Model):
+	name = models.CharField(max_length=60)
+	created = models.DateTimeField(auto_now_add=True)
+	priority = models.IntegerField(default=0)
+	difficulty = models.IntegerField(default=0)
+	done = models.BooleanField(default=False)
+
